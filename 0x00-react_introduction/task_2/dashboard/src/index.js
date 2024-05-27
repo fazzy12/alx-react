d1775e1136eb1ee0efb75f3012e6b4c0d0d06813
fazzy12 <kaluifeanyi00@gmail.com>
2024-05-27 18:23:15 +0100

    feat: Add email, password input fields, and OK button to App

---
+++ 0x00-react_introduction/task_2/dashboard/src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './App.css';
+import App from './App';
+import Notifications from './Notifications';
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <div>
+      <div id="root-notifications">
+        <Notifications />
+      </div>
+      <div id="root">
+        <App />
+      </div>
+    </div>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
