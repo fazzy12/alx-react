9cf0e73988960bb226fd0f1de7bcba9bb1370abf
fazzy12 <kaluifeanyi00@gmail.com>
2024-05-27 18:14:46 +0100

    feat: Add email, password input fields, and OK button to App

--- 0x00-react_introduction/task_1/dashboard/src/App.js
+++ 0x00-react_introduction/task_1/dashboard/src/App.js
@@ -4,17 +4,26 @@ import logo from './hoberton_logo.jpg'
 import { getFullYear, getFooterCopy } from './utils';
 
 
-function App(){
+function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>School dashboard</h1>
       </header>
-      <body className='App-body'>
+      <main className="App-body">
         <p>Login to access the full dashboard</p>
-      </body>
-      <footer className='App-footer'>
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input type="email" id="email" />
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input type="password" id="password" />
+        </div>
+        <button>OK</button>
+      </main>
+      <footer className="App-footer">
         <p>Copyright {getFullYear()} - {getFooterCopy(true)}</p>
       </footer>
     </div>
