71dd021d383bcb37b472bb43ba2ff3987011e3b4
fazzy12 <kaluifeanyi00@gmail.com>
2024-05-27 17:56:17 +0100

    feat: Add utility functions and Notifications component

---
+++ 0x00-react_introduction/task_1/dashboard/src/utils.js
@@ -0,0 +1,11 @@
+// utils.js
+
+// Function to get the current year
+export function getFullYear() {
+    return new Date().getFullYear();
+  }
+
+  // Function to get the footer copy based on isIndex
+  export function getFooterCopy(isIndex) {
+    return isIndex ? 'Holberton School' : 'Holberton School main dashboard';
+  }
