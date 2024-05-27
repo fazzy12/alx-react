234a8118785ec852d0b6c24fb2c5b9ac960bbc72
fazzy12 <kaluifeanyi00@gmail.com>
2024-05-27 16:44:26 +0100

    feat: Create basic React app with Holberton logo and footer

---
+++ 0x00-react_introduction/task_0/dashboard/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import './App.css'
+import logo from './hoberton_logo.jpg'
+
+
+function App(){
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>School dashboard</h1>
+      </header>
+      <body className='App-body'>
+        <p>Login to access the full dashboard</p>
+      </body>
+      <footer className='App-footer'>
+        <p>Copyright 2020 - holberton School</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
