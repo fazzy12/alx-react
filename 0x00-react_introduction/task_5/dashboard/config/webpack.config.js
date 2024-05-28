e2d2d51c77dda20e16f66b9df49988b1349ec6a0
fazzy12 <kaluifeanyi00@gmail.com>
2024-05-28 15:06:58 +0100

    feat: Set up project using Webpack

---
+++ 0x00-react_introduction/task_5/dashboard/config/webpack.config.js
@@ -0,0 +1,34 @@
+// webpack.config.js
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+  devServer: {
+    contentBase: './dist',
+    hot: true,
+  },
+};
