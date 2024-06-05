c3b960e2ab8ff23e43167670b2ed96de2b0f5868
fazzy12 <kaluifeanyi00@gmail.com>
2024-06-05 21:25:16 +0100

    Add tests for BodySection and BodySectionWithMarginBottom components

--- 0x03-React_component/task_3/dashboard/src/BodySection/BodySection.test.js
+++ 0x03-React_component/task_3/dashboard/src/BodySection/BodySection.test.js
@@ -16,4 +16,17 @@ describe('<BodySection />', () => {
     expect(wrapper.find('p').length).toBe(1);
     expect(wrapper.find('p').text()).toBe('test children node');
   });
+
+  it('renders correctly with children and one h2 element', () => {
+    const wrapper = shallow(
+      <BodySection title="test title">
+        <p>test children node</p>
+      </BodySection>
+    );
+
+    expect(wrapper.find('h2').length).toBe(1);
+    expect(wrapper.find('h2').text()).toBe('test title');
+    expect(wrapper.find('p').length).toBe(1);
+    expect(wrapper.find('p').text()).toBe('test children node');
+  });
 });
