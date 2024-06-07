33c268b963d2655fa205c610c4252d6bf649f651
fazzy12 <kaluifeanyi00@gmail.com>
2024-06-07 16:01:37 +0100

    feat: Use Aphrodite for Header component styling

--- 0x04-React_inline_styling/task_1/dashboard/src/Header/Header.js
+++ 0x04-React_inline_styling/task_1/dashboard/src/Header/Header.js
@@ -1,14 +1,29 @@
-
 import React from 'react';
-import logo from '../assets/hoberton_logo.jpg'
-import './Header.css'
+import { StyleSheet, css } from 'aphrodite';
+import logo from '../assets/hoberton_logo.jpg';
 
+const styles = StyleSheet.create({
+  header: {
+    display: 'flex',
+    alignItems: 'center',
+    borderBottom: '3px solid #e0354b',
+  },
+  logo: {
+    width: '200px',
+    height: '200px',
+  },
+  title: {
+    // color: '#e0354b',
+    color: 'green',
+    marginLeft: '20px',
+  },
+});
 
 function Header() {
   return (
-    <header className='App-header'>
-      <img src={logo} className='App-logo' alt="Holberton Logo" />
-      <h1>Welcome to the Dashboard</h1>
+    <header className={css(styles.header)}>
+      <img src={logo} className={css(styles.logo)} alt="Holberton Logo" />
+      <h1 className={css(styles.title)}>Welcome to the Dashboard</h1>
     </header>
   );
 }
