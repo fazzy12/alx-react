f1a24edc13fe97250b1d5fc0af03f8dffeef55b3
fazzy12 <kaluifeanyi00@gmail.com>
2024-06-07 17:57:10 +0100

    feat: Add animations to Notifications menu

--- 0x04-React_inline_styling/task_2/dashboard/src/Notifications/NotificationItem.js
+++ 0x04-React_inline_styling/task_2/dashboard/src/Notifications/NotificationItem.js
@@ -2,19 +2,50 @@ import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import { StyleSheet, css } from 'aphrodite';
 
+const bounceKeyframes = {
+  '0%': { transform: 'translateY(0px)' },
+  '50%': { transform: 'translateY(-5px)' },
+  '100%': { transform: 'translateY(5px)' },
+};
+
+const opacityKeyframes = {
+  '0%': { opacity: 0.5 },
+  '100%': { opacity: 1 },
+};
+
 const styles = StyleSheet.create({
-  defaultItem: {
-    color: 'blue',
-  },
-  urgentItem: {
-    color: 'red',
-  },
-  '@media (max-width: 900px)': {
-    item: {
-      width: '100%',
+  menuItem: {
+    textAlign: 'right',
+    cursor: 'pointer',
+    float: 'right',
+    backgroundColor: '#fff8f8',
+    ':hover': {
+      animationName: [bounceKeyframes, opacityKeyframes],
+      animationDuration: '0.5s, 1s',
+      animationIterationCount: '3, 3',
     },
   },
+  notifications: {
+    display: 'none',
+    position: 'fixed',
+    top: 0,
+    right: 0,
+    bottom: 0,
+    left: 0,
+    backgroundColor: 'white',
+    zIndex: 1000,
+    overflowY: 'scroll',
+    padding: 0,
+    fontSize: '20px',
+  },
+  showNotifications: {
+    display: 'block',
+  },
+  hideMenuItem: {
+    display: 'none',
+  },
 });
+
 
 class NotificationItem extends PureComponent {
   render() {
